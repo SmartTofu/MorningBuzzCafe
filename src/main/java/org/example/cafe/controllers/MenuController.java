d729fa26f6d02af29ae56cde4468c828a14cb26f
Yakobchuk Kirill <Kirill@zodium.ru>
2024-03-24 00:48:21 +0300

    Added 1 part of home page + controller for menu

--- src/main/java/org/example/cafe/controllers/MenuController.java
+++ src/main/java/org/example/cafe/controllers/MenuController.java
@@ -3,17 +3,11 @@ package org.example.cafe.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String main(Model model) {
-        return "home";
-    }
-
-    @GetMapping("/home.html")
+public class MenuController {
+    @GetMapping("/menu.html")
     public String home(Model model) {
-        return "home";
+        return "menu";
     }
 }
