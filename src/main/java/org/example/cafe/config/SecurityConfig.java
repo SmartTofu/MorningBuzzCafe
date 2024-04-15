84d4273848cf02bb3290d389035c1ed97d0f10df
Yakobchuk Kirill <Kirill@zodium.ru>
2024-04-16 00:07:08 +0300

    Added some security features

---
+++ src/main/java/org/example/cafe/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package org.example.cafe.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.builder().username("admin")
+                .password(encoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user1 = User.builder().username("Kirill")
+                .password(encoder.encode("kirill123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user1);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/"),
+                                new AntPathRequestMatcher("/home"),
+                                new AntPathRequestMatcher("/menu"))
+                        .permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/admin")).authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/org/example/cafe/controllers/AdminController.java
+++ src/main/java/org/example/cafe/controllers/AdminController.java
@@ -6,6 +6,7 @@ import org.example.cafe.entity.Product;
 import org.example.cafe.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 @AllArgsConstructor
 public class AdminController {
 
@@ -43,12 +45,6 @@ public class AdminController {
         ProductDTO dto = new ProductDTO(name, price, text);
         return new ResponseEntity<>(productService.create(dto), HttpStatus.OK);
     }
-
-    // ПОКА ЧТО ПО АДРЕСУ "/admin" нет обработки PUT запросов
-//    @RequestMapping(value = "/admin", method = RequestMethod.PUT)
-//    public ResponseEntity<Product> update(@RequestBody Product product) {
-//        return new ResponseEntity<>(productService.update(product), HttpStatus.OK);
-//    }
 
     @RequestMapping(value = "/admin", method = RequestMethod.DELETE)
     public void delete(@RequestParam Long delete_id, Model model) {
