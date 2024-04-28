6ebd9b5ee2a01256861f06d28dfc835c7eccfa4d
Yakobchuk Kirill <Kirill@zodium.ru>
2024-04-28 14:47:33 +0300

    Add search-feature to menu-page

--- src/main/java/org/example/cafe/controllers/MenuController.java
+++ src/main/java/org/example/cafe/controllers/MenuController.java
@@ -6,6 +6,9 @@ import org.example.cafe.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -15,9 +18,28 @@ public class MenuController {
 
     // Обработчик страницы с каталогом
     @GetMapping("/menu")
-    public String menu(Model model) {
-        Iterable<Product> positions = productService.readAll();
-        model.addAttribute("positions", positions);
+    public String menu(@RequestParam(value="ask_name", required = false) String ask_name, Model model) {
+        List<Product> positions = productService.readAll();
+        if (ask_name == null) {
+            model.addAttribute("positions", positions);
+        }
+        else {
+            Product product = findProductByName(ask_name, positions);
+            if (product == null) {
+                model.addAttribute("positions", positions);
+            }
+            else {
+                model.addAttribute("positions", product);
+            }
+        }
         return "menu";
+    }
+
+    public Product findProductByName(String name, List<Product> list) {
+        for (int i = 0; i < list.size(); ++i) {
+            if (list.get(i).getName().equals(name))
+                return list.get(i);
+        }
+        return null;
     }
 }
